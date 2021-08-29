abb9b2dacefd244df9fd630463bfdbef6beb08e7
Avaer Kazmer <a@modules.io>
2021-08-29 08:44:43 -0400

    Update jsx.js

--- types/jsx.js
+++ types/jsx.js
@@ -1,13 +1,15 @@
 const Babel = require('@babel/core');
 
-module.exports = function jsx(src, id) {
-  const spec = Babel.transform(src, {
-    presets: ['@babel/preset-react'],
-    // compact: false,
-  });
-  const {code} = spec;
-  return {
-    code,
-    map: null // provide source map if available
-  };
-}
+module.exports = {
+  transform(src, id) {
+    const spec = Babel.transform(src, {
+      presets: ['@babel/preset-react'],
+      // compact: false,
+    });
+    const {code} = spec;
+    return {
+      code,
+      map: null // provide source map if available
+    };
+  },
+};
