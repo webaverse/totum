4606c719292e54d33db49c613c3514c2f952f224
Avaer Kazmer <a@modules.io>
2021-08-29 08:44:37 -0400

    Update rollup.js

--- plugins/rollup.js
+++ plugins/rollup.js
@@ -18,19 +18,50 @@ const loaders = (() => {
   return result;
 })();
 
+const _getType = id => {
+  let match;
+  // console.log('transform', id, match);
+  if (match = id.match(/^ipfs:\/+([a-z0-9]+)((?:\/?[^\/\?]*)*)(?:\?\.([^\.]+))?$/i)) {
+    return match[3];
+  } else if (match = id.match(/(?:\.([^\.]+))$/)) {
+    return match[1];
+  } else {
+    return null;
+  }
+};
+
 module.exports = function metaversefilePlugin() {
   return {
     name: 'metaversefile',
     enforce: 'pre',
-    resolveId(source, importer) {
-      // console.log('resolveId', source);
-      if (/^ipfs:\/+/.test(source)) {
-        return source;
+    async resolveId(source, importer) {
+      const type = _getType(source);
+      const loader = type && loaders[type];
+      const resolveId = loader?.resolveId;
+      if (resolveId) {
+        return await resolveId(source, importer);
       } else {
         return null;
       }
+      
+      // console.log('resolveId', source);
+      /* if (/^ipfs:\/+/.test(source)) {
+        return source;
+      } else {
+        return null;
+      } */
     },
-    load(id) {
+    async load(id) {
+      const type = _getType(id);
+      const loader = type && loaders[type];
+      const load = loader?.load;
+      if (load) {
+        const src = await load(id);
+        if (typeof newId === 'string') {
+          return src;
+        }
+      }
+      
       const match = id.match(/^ipfs:\/+([a-z0-9]+)((?:\/?[^\/\?]*)*)(\?\.(.+))?$/i);
       // console.log('load', id, !!match);
       if (match) {
@@ -40,15 +71,13 @@ module.exports = function metaversefilePlugin() {
         return null;
       }
     },
-    transform(src, id) {
-      const match = id.match(/^ipfs:\/+([a-z0-9]+)((?:\/?[^\/\?]*)*)(\?\.(.+))?$/i);
-      // console.log('transform', id, match);
-      if (match) {
-        const type = match[3];
-        const loader = loaders[type];
-        if (loader) {
-          return loader(src, id);
-        }
+    async transform(src, id) {
+      const type = _getType(id);
+      // console.log('get type', {id, type});
+      const loader = type && loaders[type];
+      const transform = loader?.transform;
+      if (transform) {
+        return await transform(src, id);
       }
     }
   }
