db3bd71ce3aa41aa7f82a327eaee86e21a8fd3a0
Avaer Kazmer <a@modules.io>
2021-08-30 14:34:28 -0400

    Create vrm.js

---
+++ types/vrm.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const fs = require('fs');
+const {fillTemplate} = require('../util.js');
+
+const templateString = fs.readFileSync(path.join(__dirname, '..', 'type_templates', 'vrm.js'));
+const cwd = process.cwd();
+
+module.exports = {
+  load(id) {
+    if (id.startsWith(cwd)) {
+      id = id.slice(cwd.length);
+    }
+    console.log('load vrm', id);
+    const code = fillTemplate(templateString, {
+      srcUrl: id,
+    });
+    // console.log('got glb id', id);
+    return {
+      code,
+      map: null,
+    };
+  },
+};
