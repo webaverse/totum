18810ed991b4ae42bb042d2065aa701d90d260c5
Avaer Kazmer <a@modules.io>
2021-09-04 08:22:00 -0400

    Update metaversefile.js

--- types/metaversefile.js
+++ types/metaversefile.js
@@ -24,10 +24,10 @@ const isSubpath = (parent, dir) => {
 module.exports = {
   async resolveId(id, importer) {
     const s = await fetchFileFromId(id, importer, 'utf8');
-    console.log('metaversefile fetch', {id, importer, s});
+    // console.log('metaversefile fetch', {id, importer, s});
     if (s !== null) {
       const j = _jsonParse(s);
-      console.log('load metaversefile', {s}, j);
+      // console.log('load metaversefile', {s}, j);
       const start_url = j?.start_url;
       if (start_url) {
         if (id.startsWith(cwd)) {
@@ -35,13 +35,13 @@ module.exports = {
         }
         
         const o = url.parse(id);
-        console.log('new metaversefile id 1', {id, importer, start_url, o}, [path.dirname(o.pathname), start_url]);
+        // console.log('new metaversefile id 1', {id, importer, start_url, o}, [path.dirname(o.pathname), start_url]);
         o.pathname = path.join(path.dirname(o.pathname), start_url);
         let s = url.format(o);
         if (/^\//.test(s)) {
           s = cwd + s;
         }
-        console.log('new metaversefile id 2', {id, importer, start_url, o, s}, [path.dirname(o.pathname), start_url]);
+        // console.log('new metaversefile id 2', {id, importer, start_url, o, s}, [path.dirname(o.pathname), start_url]);
         return s;
       } else {
         console.warn('.metaversefile has no "start_url": string', {j, id, s});
