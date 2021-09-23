2be856d9c62308429d1c563b199c702ed80c4502
Avaer Kazmer <a@modules.io>
2021-09-23 05:14:56 -0400

    Make wassies transparent

--- contract_templates/loomlock.js
+++ contract_templates/loomlock.js
@@ -25,6 +25,7 @@ export default e => {
     const material = new THREE.MeshBasicMaterial({
       map: texture,
       side: THREE.DoubleSide,
+      transparent: true,
     });
     const imageMesh = new THREE.Mesh(geometry, material);
     (async () => {
@@ -42,7 +43,58 @@ export default e => {
         img.crossOrigin = 'Aynonymous';
         img.src = j.image;
       });
-      texture.image = img;
+      
+      const canvas = document.createElement('canvas');
+      canvas.width = img.naturalWidth;
+      canvas.height = img.naturalHeight;
+      const ctx = canvas.getContext('2d');
+      ctx.drawImage(img, 0, 0);
+      const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+      const queue = [[0, 0]];
+      const seen = {};
+      const _getKey = (x, y) => x + ':' + y;
+      while (queue.length > 0) {
+        const [x, y] = queue.pop();
+        const k = _getKey(x, y);
+        if (!seen[k]) {
+          seen[k] = true;
+          
+          const startIndex = y*imageData.width*4 + x*4;
+          const endIndex = startIndex + 4;
+          const [r, g, b, a] = imageData.data.slice(startIndex, endIndex);
+          if (r < 255/6 && g < 255/6 && b < 255/6) {
+            // nothing
+          } else {
+            imageData.data[startIndex] = 0;
+            imageData.data[startIndex+1] = 0;
+            imageData.data[startIndex+2] = 0;
+            imageData.data[startIndex+3] = 0;
+            
+            const _tryQueue = (x, y) => {
+              if (x >= 0 && x < canvas.width && y >= 0 && y < canvas.height) {
+                const k = _getKey(x, y);
+                if (!seen[k]) {
+                  queue.push([x, y]);
+                }
+              }
+            };
+            _tryQueue(x-1, y-1);
+            _tryQueue(x,   y-1);
+            _tryQueue(x+1, y-1);
+            
+            _tryQueue(x-1, y);
+            // _tryQueue(x, y);
+            _tryQueue(x+1, y);
+            
+            _tryQueue(x-1, y+1);
+            _tryQueue(x,   y+1);
+            _tryQueue(x+1, y+1);
+          }
+        }
+      }
+      ctx.putImageData(imageData, 0, 0);
+      
+      texture.image = canvas;
       texture.needsUpdate = true;
     })();
     
