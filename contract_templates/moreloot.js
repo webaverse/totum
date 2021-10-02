baa82017273b82cd18ae749a2e5e6de12b0ba4cb
Avaer Kazmer <a@modules.io>
2021-10-02 03:11:37 -0700

    Loot cleanup

--- contract_templates/moreloot.js
+++ contract_templates/moreloot.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import metaversefile from 'metaversefile';
-const {useApp, removeApp, useLocalPlayer, useLoaders, useCleanup, usePhysics, useWeb3, useAbis, useWorld} = metaversefile;
+const {useApp, removeApp, useLocalPlayer, useActivate, useLoaders, useCleanup, usePhysics, useWeb3, useAbis, useWorld} = metaversefile;
 
 const _capitalize = s => s.slice(0, 1).toUpperCase() + s.slice(1);
 const _capitalizeWords = s => {
