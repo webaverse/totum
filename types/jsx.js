b0be319c38c4bd37cae69fa3508e92d153b14ead
Avaer Kazmer <a@modules.io>
2021-11-04 10:18:24 -0400

    Support components passthrough in jsx types handler

--- types/jsx.js
+++ types/jsx.js
@@ -1,12 +1,40 @@
 const Babel = require('@babel/core');
 
+function parseQuery(queryString) {
+  const query = {};
+  const pairs = (queryString[0] === '?' ? queryString.substr(1) : queryString).split('&');
+  for (let i = 0; i < pairs.length; i++) {
+    const pair = pairs[i].split('=');
+    const k = decodeURIComponent(pair[0]);
+    if (k) {
+      const v = decodeURIComponent(pair[1] || '');
+      query[k] = v;
+    }
+  }
+  return query;
+}
+
 module.exports = {
   transform(src, id) {
+    const components = (() => {
+      const match = id.match(/#([\s\S]+)$/);
+      if (match) {
+        const q = parseQuery(match[1]);
+        return q.components !== undefined ? JSON.parse(q.components) : [];
+      } else {
+        return [];
+      }
+    })();
+    
     const spec = Babel.transform(src, {
       presets: ['@babel/preset-react'],
       // compact: false,
     });
-    const {code} = spec;
+    let {code} = spec;
+    code += `
+
+export const components = ${JSON.stringify(components)};
+`;
     return {
       code,
       map: null,
