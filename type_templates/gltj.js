f2faba31119387b37e29d007d4641a8c91ba6971
Avaer Kazmer <a@modules.io>
2021-11-08 17:28:13 -0500

    Bugfix iResolution injection for gltj case

--- type_templates/gltj.js
+++ type_templates/gltj.js
@@ -1,7 +1,8 @@
 import * as THREE from 'three';
 import metaversefile from 'metaversefile';
-const {useApp, useFrame, useLoaders, useScene, usePhysics, useJSON6Internal, useCleanup} = metaversefile;
+const {useApp, useFrame, useLoaders, useScene, usePhysics, useInternals, useJSON6Internal, useCleanup} = metaversefile;
 
+const {renderer} = useInternals();
 const JSON6 = useJSON6Internal();
 const geometry = new THREE.PlaneBufferGeometry(2, 2);
 
@@ -28,6 +29,14 @@ export default e => {
       if (material.uniforms.iTime) {
         material.uniforms.iTime.value = now/1000;
         material.uniforms.iTime.needsUpdate = true;
+      }
+      if (material.uniforms.iResolution) {
+        if (!material.uniforms.iResolution.value) {
+          material.uniforms.iResolution.value = new THREE.Vector2();
+        }
+        renderer.getSize(material.uniforms.iResolution.value)
+          .multiplyScalar(renderer.getPixelRatio());
+        material.uniforms.iResolution.needsUpdate = true;
       }
       
       now += timeDiff;
