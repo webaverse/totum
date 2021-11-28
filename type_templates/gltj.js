3f9d6221ab705dc40a974f90047bf0f58ef80d47
Sunnan Islam Rahi <sirahi97@gmail.com>
2021-11-29 01:46:17 +0500

    feat: gltj set uniforms from scn

--- type_templates/gltj.js
+++ type_templates/gltj.js
@@ -11,41 +11,46 @@ export default e => {
   app.appType = 'gltj';
 
   const srcUrl = '${this.srcUrl}';
-  
+
   let _update = null;
   e.waitUntil((async () => {
-    const res = await fetch(srcUrl);
-    const s = await res.text();
-    const j = JSON6.parse(s);
-    
-    const material = new THREE.ShaderMaterial(j);
-    
-    const mesh = new THREE.Mesh(geometry, material);
-    mesh.frustumCulled = false;
-    app.add(mesh);
-    
-    let now = 0;
-    _update = timeDiff => {
-      if (material.uniforms.iTime) {
-        material.uniforms.iTime.value = now/1000;
-        material.uniforms.iTime.needsUpdate = true;
-      }
-      if (material.uniforms.iResolution) {
-        if (!material.uniforms.iResolution.value) {
-          material.uniforms.iResolution.value = new THREE.Vector2();
-        }
-        renderer.getSize(material.uniforms.iResolution.value)
-          .multiplyScalar(renderer.getPixelRatio());
-        material.uniforms.iResolution.needsUpdate = true;
-      }
-      
-      now += timeDiff;
-    };
-  })());
+      const res = await fetch(srcUrl);
+      const s = await res.text();
+      const j = JSON6.parse(s);
 
-  useFrame(({timeDiff}) => {
-    _update && _update(timeDiff);
-  });
+      const material = new THREE.ShaderMaterial(j);
+
+      const mesh = new THREE.Mesh(geometry, material);
+      mesh.frustumCulled = false;
+      app.add(mesh);
+
+      const uniforms = app.getComponent("uniforms");
+      for (const name in uniforms) {
+        material.uniforms[name].value = uniforms[name];
+      }
+
+      let now = 0;
+      _update = timeDiff => {
+        if (material.uniforms.iTime) {
+          material.uniforms.iTime.value = now/1000;
+          material.uniforms.iTime.needsUpdate = true;
+        }
+        if (material.uniforms.iResolution) {
+          if (!material.uniforms.iResolution.value) {
+            material.uniforms.iResolution.value = new THREE.Vector2();
+          }
+          renderer.getSize(material.uniforms.iResolution.value)
+            .multiplyScalar(renderer.getPixelRatio());
+          material.uniforms.iResolution.needsUpdate = true;
+        }
+
+        now += timeDiff;
+      };
+    })());
+
+    useFrame(({timeDiff}) => {
+      _update && _update(timeDiff);
+    });
 
   return app;
 };
