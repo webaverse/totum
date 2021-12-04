cd42bc2d57cf9b241aeb0cd8b5d52e7e2892ffa5
yaser <yaser@superblacksoftware.net>
2021-12-04 17:15:21 +0000

    shadertoy renderer mesh and vox

--- type_templates/glbb.js
+++ type_templates/glbb.js
@@ -25,6 +25,7 @@ export default () => {
     });
     // await shadertoyRenderer.waitForLoad();
     o.add(shadertoyRenderer.mesh);
+    shadertoyRenderer.mesh.updateMatrixWorld();
     _update = timeDiff => {
       shadertoyRenderer.update(timeDiff/1000);
     };
