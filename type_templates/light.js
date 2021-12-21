255319d5f1f0046108720ae058330d8656cedeb8
Avaer Kazmer <a@modules.io>
2021-12-21 00:59:26 -0500

    Small cleanup

--- type_templates/light.js
+++ type_templates/light.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import metaversefile from 'metaversefile';
-const {useApp, useFrame, useLocalPlayer, useCleanup, usePhysics, useWorld} = metaversefile;
+const {useApp, useFrame, useLocalPlayer, useCleanup, /*usePhysics, */ useWorld} = metaversefile;
 
 const localVector = new THREE.Vector3();
 const localVector2 = new THREE.Vector3();
@@ -52,7 +52,6 @@ export default e => {
     // console.log("Added shadows for:", light, "with params:", params);
   };
 
-  
   const lightTrackers = [];
   const lightTargets = [];
   e.waitUntil((async () => {
