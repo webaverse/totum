c2f6495917fa3dd78728649c5fb30c3eebc2a9c9
Avaer Kazmer <a@modules.io>
2022-02-21 17:04:25 -0500

    Clean up vrm types

--- types/vrm.js
+++ types/vrm.js
@@ -3,39 +3,41 @@ const fs = require('fs');
 const {fillTemplate, createRelativeFromAbsolutePath} = require('../util.js');
 
 const templateString = fs.readFileSync(path.join(__dirname, '..', 'type_templates', 'vrm.js'), 'utf8');
-const cwd = process.cwd();
+// const cwd = process.cwd();
 
-function parseQuery(queryString) {
-    const query = {};
-    const pairs = (queryString[0] === '?' ? queryString.substr(1) : queryString).split('&');
-    for (let i = 0; i < pairs.length; i++) {
-      const pair = pairs[i].split('=');
-      const k = decodeURIComponent(pair[0]);
-      if (k) {
-        const v = decodeURIComponent(pair[1] || '');
-        query[k] = v;
-      }
+/* function parseQuery(queryString) {
+  const query = {};
+  const pairs = (queryString[0] === '?' ? queryString.substr(1) : queryString).split('&');
+  for (let i = 0; i < pairs.length; i++) {
+    const pair = pairs[i].split('=');
+    const k = decodeURIComponent(pair[0]);
+    if (k) {
+      const v = decodeURIComponent(pair[1] || '');
+      query[k] = v;
     }
-    return query;
   }
+  return query;
+} */
   
 module.exports = {
   load(id) {
-    
     id = createRelativeFromAbsolutePath(id);
 
-    const components = (() => {
-        const match = id.match(/#([\s\S]+)$/);
-        if (match) {
-          const q = parseQuery(match[1]);
-          return q.components !== undefined ? JSON.parse(q.components) : [];
-        } else {
-          return [];
+    let components = [];
+    (() => {
+      const match = id.match(/#([\s\S]+)$/);
+      if (match) {
+        const q = new URLSearchParams(match[1]);
+        const qComponents = q.get('components');
+        if (qComponents !== undefined) {
+          components = jsonParse(qComponents) ?? [];
         }
+      }
     })();
+
     const code = fillTemplate(templateString, {
-        srcUrl: id,
-        components: JSON.stringify(components),
+      srcUrl: JSON.stringify(id),
+      components: JSON.stringify(components),
     });
     return {
       code,
