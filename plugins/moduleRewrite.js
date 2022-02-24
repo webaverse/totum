8580ed63960617d6800e548ccce75c92adc45e25
Muhammad Abeer <abeersaqib948@gmail.com>
2022-02-24 23:39:46 +0500

    fixes

--- plugins/moduleRewrite.js
+++ plugins/moduleRewrite.js
@@ -1,9 +1,9 @@
 module.exports = function replaceImport(originalPath, callingFileName, options) {
   console.log(originalPath);
   if (originalPath === 'three' || originalPath.includes('three/examples')) {
-    originalPath = originalPath.replace('three', `${process.env.MODULE_URL}three.js`);
+    originalPath = `${process.env.MODULE_URL}three-proxy.js`;
   } else if (originalPath === 'metaversefile') {
-    originalPath = originalPath.replace('metaversefile', `${process.env.MODULE_URL}metaversefile.js`);
+    originalPath = originalPath.replace('metaversefile', `${process.env.MODULE_URL}metaversefile-proxy.js`);
   } else if (originalPath.includes('three-vrm')) {
     originalPath = `${process.env.MODULE_URL}three-vrm.js`;
   }
