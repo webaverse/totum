933564292e3077b9c17d3ddbb34280c0a0522e68
Muhammad Abeer <abeersaqib948@gmail.com>
2022-02-27 02:21:20 +0500

    added proxy option in module reWrite

--- plugins/moduleRewrite.js
+++ plugins/moduleRewrite.js
@@ -1,9 +1,12 @@
+const proxy = '-proxy';
+
 module.exports = function replaceImport(originalPath, callingFileName, options) {
-  console.log(originalPath);
   if (originalPath === 'three' || originalPath.includes('three/examples')) {
-    originalPath = `${process.env.MODULE_URL}three-proxy.js`;
+    originalPath = `${process.env.MODULE_URL}three${proxy}.js`;
+  } else if (originalPath.includes('three/examples')) {
+    originalPath = `${process.env.MODULE_URL}three-examples${proxy}.js`;
   } else if (originalPath === 'metaversefile') {
-    originalPath = originalPath.replace('metaversefile', `${process.env.MODULE_URL}metaversefile-proxy.js`);
+    originalPath = originalPath.replace('metaversefile', `${process.env.MODULE_URL}metaversefile.js`);
   } else if (originalPath.includes('three-vrm')) {
     originalPath = `${process.env.MODULE_URL}three-vrm.js`;
   }
