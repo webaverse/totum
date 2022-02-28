506f590d80d6039a99159963c782ac3c5167340b
Avaer Kazmer <a@modules.io>
2022-02-28 05:00:56 -0500

    Add metadata to vox

--- types/vox.js
+++ types/vox.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const fs = require('fs');
-const {fillTemplate, createRelativeFromAbsolutePath} = require('../util.js');
+const {fillTemplate, createRelativeFromAbsolutePath, parseIdHash} = require('../util.js');
 
 const templateString = fs.readFileSync(path.join(__dirname, '..', 'type_templates', 'vox.js'), 'utf8');
 // const cwd = process.cwd();
@@ -8,10 +8,22 @@ const templateString = fs.readFileSync(path.join(__dirname, '..', 'type_template
 module.exports = {
   load(id) {
     id = createRelativeFromAbsolutePath(id);
+
+    const {
+      contentId,
+      name,
+      description,
+      components,
+    } = parseIdHash(id);
     
     const code = fillTemplate(templateString, {
       srcUrl: JSON.stringify(id),
+      contentId: JSON.stringify(contentId),
+      name: JSON.stringify(name),
+      description: JSON.stringify(description),
+      components: JSON.stringify(components),
     });
+
     return {
       code,
       map: null,
