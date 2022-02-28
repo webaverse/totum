109f12a558c5c83074bd4880a696f5257e4c0a0b
Avaer Kazmer <a@modules.io>
2022-02-28 05:00:43 -0500

    Add metadata to text

--- types/text.js
+++ types/text.js
@@ -9,10 +9,22 @@ module.exports = {
   load(id) {
 
     id = createRelativeFromAbsolutePath(id);
+
+    const {
+      contentId,
+      name,
+      description,
+      components,
+    } = parseIdHash(id);
     
     const code = fillTemplate(templateString, {
       srcUrl: JSON.stringify(id),
+      contentId: JSON.stringify(contentId),
+      name: JSON.stringify(name),
+      description: JSON.stringify(description),
+      components: JSON.stringify(components),
     });
+
     return {
       code,
       map: null,
