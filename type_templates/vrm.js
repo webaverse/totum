057e80e65a04a53603fe683463509da3fa12af2f
0reo <oreo@halfbakedideals.com>
2022-03-04 13:40:02 -0500

    remove unneeded unskin code

--- type_templates/vrm.js
+++ type_templates/vrm.js
@@ -76,50 +76,13 @@ const _addAnisotropy = (o, anisotropyLevel) => {
     }
   });
 };
-const _unskin = o => { // process avatar to elide expensive bone updates
-  const skinnedMeshes = [];
-  o.traverse(o => {
-    if (o.isSkinnedMesh) {
-      skinnedMeshes.push(o);
-    }
-  });
 
-  for (const skinnedMesh of skinnedMeshes) {
-    const {geometry, material, position, quaternion, scale, matrix, matrixWorld, visible, parent} = skinnedMesh;
-    const mesh = new THREE.Mesh(geometry, material);
-    mesh.castShadow = true;
-    mesh.receiveShadow = true;
-    mesh.position.copy(position);
-    mesh.quaternion.copy(quaternion);
-    mesh.scale.copy(scale);
-    mesh.matrix.copy(matrix);
-    mesh.matrixWorld.copy(matrixWorld);
-    mesh.visible = visible;
-    mesh.parent = parent;
-    const index = parent ? parent.children.indexOf(skinnedMesh) : -1;
-    if (index !== -1) {
-      parent.children.splice(index, 1, mesh);
-    }
-  }
-  /* for (const skinnedMesh of skinnedMeshes) {
-    const skeleton = skinnedMesh.skeleton;
-    for (const bone of skeleton.bones) {
-      if (bone.parent && !bone.parent.isBone) {
-        bone.oldParent = bone.parent;
-        bone.parent.remove(bone);
-      }
-    }
-  } */
-};
 
 export default e => {
   const physics = usePhysics();
   
   const app = useApp();
   app.appType = 'vrm';
-  
-  app.skinnedVrm = null;
-  app.unskinnedVrm = null;
   
   const srcUrl = ${this.srcUrl};
   for (const {key, value} of components) {
@@ -132,50 +95,51 @@ export default e => {
     vrm.cloneVrm = _cloneVrm;
     return vrm;
   };
-
+  
+  const _prepVrm = (vrm) => {
+    //vrm.visible = false; //will need later
+    app.add(vrm);
+    vrm.updateMatrixWorld();
+    _addAnisotropy(vrm, 16);
+  }
+  
   let physicsIds = [];
   let activateCb = null;
   e.waitUntil((async () => {
     arrayBuffer = await _fetchArrayBuffer(srcUrl);
-
-    const unskinnedVrm = await _cloneVrm();
-    if (unskinnedVrm) {
-      await _toonShaderify(unskinnedVrm);
-      app.unskinnedVrm = unskinnedVrm;
-
-      app.add(unskinnedVrm.scene);
-      unskinnedVrm.scene.updateMatrixWorld();
-      
-      _addAnisotropy(unskinnedVrm.scene, 16);
-      _unskin(unskinnedVrm.scene);
-
-      const _addPhysics = () => {
-        const fakeHeight = 1.5;
-        localMatrix.compose(
-          localVector.set(0, fakeHeight/2, 0),
-          localQuaternion.identity(),
-          localVector2.set(0.3, fakeHeight/2, 0.3)
+    
+    const skinnedVrmBase = await _cloneVrm();
+    app.skinnedVrm = skinnedVrmBase;    
+    await _toonShaderify(skinnedVrmBase);
+    _prepVrm(skinnedVrmBase.scene);
+     
+    const _addPhysics = () => {
+      const fakeHeight = 1.5;
+      localMatrix.compose(
+        localVector.set(0, fakeHeight/2, 0),
+        localQuaternion.identity(),
+        localVector2.set(0.3, fakeHeight/2, 0.3)
         )
         .premultiply(app.matrixWorld)
         .decompose(localVector, localQuaternion, localVector2);
-
-        const physicsId = physics.addBoxGeometry(
-          localVector,
-          localQuaternion,
-          localVector2,
-          false
-        );
-        physicsIds.push(physicsId);
-      };
-      if (app.getComponent('physics')) {
-        _addPhysics();
-      }
-      
-      activateCb = async () => {
-        const localPlayer = useLocalPlayer();
-        localPlayer.setAvatarApp(app);
-      };
+        
+      const physicsId = physics.addBoxGeometry(
+        localVector,
+        localQuaternion,
+        localVector2,
+        false
+      );
+      physicsIds.push(physicsId);
+    };
+    if (app.getComponent('physics')) {
+      _addPhysics();
     }
+    
+    activateCb = async () => {
+      const localPlayer = useLocalPlayer();
+      localPlayer.setAvatarApp(app);
+    };
+
   })());
   
   useActivate(() => {
@@ -186,43 +150,6 @@ export default e => {
     lookAt.apply(this, arguments);
     this.quaternion.premultiply(q180);
   })(app.lookAt);
-
-  let skinned = false;
-  app.setSkinning = async skinning => {
-    if (skinning && !skinned) {
-      if (!app.skinnedVrm) {
-        app.skinnedVrm = await _cloneVrm();
-        await _toonShaderify(app.skinnedVrm);
-      }
-
-      for (const physicsId of physicsIds) {
-        physics.disableGeometry(physicsId);
-        physics.disableGeometryQueries(physicsId);
-      }
-
-      app.unskinnedVrm.scene.parent.remove(app.unskinnedVrm.scene);
-      
-      app.position.set(0, 0, 0);
-      app.quaternion.identity();
-      app.scale.set(1, 1, 1);
-      app.updateMatrixWorld();
-      
-      app.add(app.skinnedVrm.scene);
-      
-      skinned = true;
-    } else if (!skinning && skinned) {
-      app.skinnedVrm.scene.parent.remove(app.skinnedVrm.scene);
-      
-      for (const physicsId of physicsIds) {
-        physics.enableGeometry(physicsId);
-        physics.enableGeometryQueries(physicsId);
-      }
-      
-      app.add(app.unskinnedVrm.scene);
-      
-      skinned = false;
-    }
-  }
 
   useCleanup(() => {
     for (const physicsId of physicsIds) {
