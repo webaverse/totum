e173578545ac5bec2b498a69e1cbe44f33f11b51
0reo <oreo@halfbakedideals.com>
2022-03-07 15:56:27 -0500

    add toggleBoneUpdates to prevent performance issues

--- type_templates/vrm.js
+++ type_templates/vrm.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 import metaversefile from 'metaversefile';
 import { VRMMaterialImporter } from '@pixiv/three-vrm/lib/three-vrm.module';
-const {useApp, useLoaders, usePhysics, useCleanup, useActivate, useLocalPlayer} = metaversefile;
+const { useApp, useLoaders, usePhysics, useCleanup, useActivate, useLocalPlayer } = metaversefile;
 
 const localVector = new THREE.Vector3();
 const localVector2 = new THREE.Vector3();
@@ -37,7 +37,7 @@ const _fetchArrayBuffer = async srcUrl => {
   return vrmObject;
 }; */
 const parseVrm = (arrayBuffer, srcUrl) => new Promise((accept, reject) => {
-  const {gltfLoader} = useLoaders();
+  const { gltfLoader } = useLoaders();
   gltfLoader.parse(arrayBuffer, srcUrl, accept, reject);
 });
 /* const _findMaterialsObjects = (o, name) => {
@@ -80,12 +80,12 @@ const _addAnisotropy = (o, anisotropyLevel) => {
 
 export default e => {
   const physics = usePhysics();
-  
+
   const app = useApp();
   app.appType = 'vrm';
-  
-  const srcUrl = ${this.srcUrl};
-  for (const {key, value} of components) {
+
+  const srcUrl = ${ this.srcUrl };
+  for (const { key, value } of components) {
     app.setComponent(key, value);
   }
 
@@ -95,34 +95,34 @@ export default e => {
     vrm.cloneVrm = _cloneVrm;
     return vrm;
   };
-  
+
   const _prepVrm = (vrm) => {
     //vrm.visible = false; //will need later
     app.add(vrm);
     vrm.updateMatrixWorld();
     _addAnisotropy(vrm, 16);
   }
-  
+
   let physicsIds = [];
   let activateCb = null;
   e.waitUntil((async () => {
     arrayBuffer = await _fetchArrayBuffer(srcUrl);
-    
+
     const skinnedVrmBase = await _cloneVrm();
-    app.skinnedVrm = skinnedVrmBase;    
+    app.skinnedVrm = skinnedVrmBase;
     await _toonShaderify(skinnedVrmBase);
     _prepVrm(skinnedVrmBase.scene);
-     
+
     const _addPhysics = () => {
       const fakeHeight = 1.5;
       localMatrix.compose(
-        localVector.set(0, fakeHeight/2, 0),
+        localVector.set(0, fakeHeight / 2, 0),
         localQuaternion.identity(),
-        localVector2.set(0.3, fakeHeight/2, 0.3)
-        )
+        localVector2.set(0.3, fakeHeight / 2, 0.3)
+      )
         .premultiply(app.matrixWorld)
         .decompose(localVector, localQuaternion, localVector2);
-        
+
       const physicsId = physics.addBoxGeometry(
         localVector,
         localQuaternion,
@@ -134,22 +134,48 @@ export default e => {
     if (app.getComponent('physics')) {
       _addPhysics();
     }
-    
+
+    // we don't want to have per-frame bone updates for unworn avatars
+    // so we toggle bone updates off and let the app enable them when worn
+    app.toggleBoneUpdates(false);
+
     activateCb = async () => {
       const localPlayer = useLocalPlayer();
       localPlayer.setAvatarApp(app);
     };
 
   })());
-  
+
   useActivate(() => {
     activateCb && activateCb();
   });
-  
-  app.lookAt = (lookAt => function(p) {
+
+  app.lookAt = (lookAt => function (p) {
     lookAt.apply(this, arguments);
     this.quaternion.premultiply(q180);
   })(app.lookAt);
+
+  app.setSkinning = async skinning => {
+    console.warn("WARNING: setSkinning FUNCTION IS DEPRICATED and will be removed. Please use toggleBoneUpdates instead.");
+    app.toggleBoneUpdates(skinning);
+  }
+
+  app.toggleBoneUpdates = update => {
+
+    const scene = app.skinnedVrm.scene;
+    scene.traverse(o => {
+      // o.matrixAutoUpdate = update;
+      if (o.isBone) o.matrixAutoUpdate = update;
+    });
+
+    if (update) {
+      app.position.set(0, 0, 0);
+      app.quaternion.identity();
+      app.scale.set(1, 1, 1);
+      app.updateMatrixWorld();
+    }
+
+  }
 
   useCleanup(() => {
     for (const physicsId of physicsIds) {
@@ -160,7 +186,7 @@ export default e => {
 
   return app;
 };
-export const contentId = ${this.contentId};
-export const name = ${this.name};
-export const description = ${this.description};
-export const components = ${this.components};
+export const contentId = ${ this.contentId };
+export const name = ${ this.name };
+export const description = ${ this.description };
+export const components = ${ this.components };
