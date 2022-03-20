3929bf54c01c2c21d7c16cc93fccdce51701ca93
Muhammad Abeer <abeersaqib948@gmail.com>
2022-03-21 00:34:13 +0500

    module rewrite fixes

--- plugins/moduleRewrite.js
+++ plugins/moduleRewrite.js
@@ -1,23 +1,15 @@
 const path = require('path');
-const proxy = '-proxy';
+const _exports = require('../../../dist/exports.json');
 
 module.exports = function replaceImport(originalPath, callingFileName, options) {
   /** remove extension to normalise the path */
-
-  if (originalPath === 'three') {
-    originalPath = `${process.env.MODULE_URL}three${proxy}.js`;
-  } else if (originalPath.includes('three/examples')) {
-    originalPath = `${process.env.MODULE_URL}${originalPath}.js`;
-  } else if (originalPath === 'metaversefile') {
-    originalPath = originalPath.replace('metaversefile', `${process.env.MODULE_URL}metaversefile${proxy}.js`);
-  } else if (originalPath.includes('three-vrm')) {
-    originalPath = `${process.env.MODULE_URL}three-vrm${proxy}.js`;
-  }
-
+  // let parsedPath = path.parse(originalPath);
+  const replacedPath = _exports[originalPath] || _exports[originalPath + '.js'];
+  originalPath = replacedPath ? `${process.env.MODULE_URL}${replacedPath}` : originalPath;
   const ext = path.parse(originalPath).ext;
   if (ext) {
     /** remove double extension from path */
-    originalPath = originalPath.replace(ext+ext, ext);
+    originalPath = originalPath.replace(ext + ext, ext);
   }
 
   return originalPath;
