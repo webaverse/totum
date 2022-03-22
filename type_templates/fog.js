a878818c8cfa469fed883634c7367020a1e38790
Avaer Kazmer <a@modules.io>
2022-03-21 21:07:21 -0400

    Add attached guard to fog type template

--- type_templates/fog.js
+++ type_templates/fog.js
@@ -4,35 +4,38 @@ const {useApp, useInternals, useCleanup} = metaversefile;
 
 export default e => {
   const app = useApp();
+  const {rootScene} = useInternals();
+
   app.appType = 'fog';
 
   const srcUrl = ${this.srcUrl};
+  const mode = app.getComponent('mode') ?? 'attached';
 
-  const {rootScene} = useInternals();
-
-  let live = true;
-  (async () => {
-    const res = await fetch(srcUrl);
-    if (!live) return;
-    const fog = await res.json();
-    // console.log('got fog', fog);
-    if (!live) return;
-    if (fog.fogType === 'linear') {
-      const {args = []} = fog;
-      rootScene.fog = new THREE.Fog(new THREE.Color(args[0][0]/255, args[0][1]/255, args[0][2]/255).getHex(), args[1], args[2]);
-    } else if (fog.fogType === 'exp') {
-      const {args = []} = fog;
-      // console.log('got fog args', {fog, args});
-      rootScene.fog = new THREE.FogExp2(new THREE.Color(args[0][0]/255, args[0][1]/255, args[0][2]/255).getHex(), args[1]);
-    } else {
-      console.warn('unknown fog type:', fog.fogType);
-    }
-  })();
-  
-  useCleanup(() => {
-    live = false;
-    rootScene.fog = null;
-  });
+  if (mode === 'attached') {
+    let live = true;
+    (async () => {
+      const res = await fetch(srcUrl);
+      if (!live) return;
+      const fog = await res.json();
+      // console.log('got fog', fog);
+      if (!live) return;
+      if (fog.fogType === 'linear') {
+        const {args = []} = fog;
+        rootScene.fog = new THREE.Fog(new THREE.Color(args[0][0]/255, args[0][1]/255, args[0][2]/255).getHex(), args[1], args[2]);
+      } else if (fog.fogType === 'exp') {
+        const {args = []} = fog;
+        // console.log('got fog args', {fog, args});
+        rootScene.fog = new THREE.FogExp2(new THREE.Color(args[0][0]/255, args[0][1]/255, args[0][2]/255).getHex(), args[1]);
+      } else {
+        console.warn('unknown fog type:', fog.fogType);
+      }
+    })();
+    
+    useCleanup(() => {
+      live = false;
+      rootScene.fog = null;
+    });
+  }
 
   return app;
 };
