2f0c3817071ac29ffbb375491eec88e7dbb5e181
Avaer Kazmer <a@modules.io>
2022-03-21 21:11:01 -0400

    Guard spawnpoint app on attached

--- type_templates/spawnpoint.js
+++ type_templates/spawnpoint.js
@@ -7,27 +7,30 @@ export default e => {
   app.appType = 'spawnpoint';
   
   const srcUrl = ${this.srcUrl};
+  const mode = app.getComponent('mode') ?? 'attached';
+  console.log('spawn point mode', app.getComponent('mode'), mode);
+  if (mode === 'attached') {
+    (async () => {
+      const res = await fetch(srcUrl);
+      const j = await res.json();
+      if (j) {
+        
+        const localPlayer = useLocalPlayer();
+        const {camera} = useInternals();
 
-  (async () => {
-    const res = await fetch(srcUrl);
-    const j = await res.json();
-    if (j) {
-      
-      const localPlayer = useLocalPlayer();
-      const {camera} = useInternals();
-
-      if (j.position) {
-        localPlayer.position.fromArray(j.position);
+        if (j.position) {
+          localPlayer.position.fromArray(j.position);
+        }
+        if (j.quaternion) {
+          localPlayer.quaternion.fromArray(j.quaternion);
+          camera.quaternion.fromArray(j.quaternion);
+        }
       }
-      if (j.quaternion) {
-        localPlayer.quaternion.fromArray(j.quaternion);
-        camera.quaternion.fromArray(j.quaternion);
-      }
-    }
-  })();
-  
-  useCleanup(() => {
-  });
+    })();
+    
+    /* useCleanup(() => {
+    }); */
+  }
 
   return app;
 };
