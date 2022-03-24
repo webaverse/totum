8e2e40b743efb8a24a64761d0a5355af3a43f725
Avaer Kazmer <a@modules.io>
2022-03-24 02:56:09 -0400

    Add paused tracking in scn type template

--- type_templates/scn.js
+++ type_templates/scn.js
@@ -51,6 +51,7 @@ export default e => {
   
   const srcUrl = ${this.srcUrl};
   const mode = app.getComponent('mode') ?? 'attached';
+  const paused = app.getComponent('paused') ?? false;
   const objectComponents = app.getComponent('objectComponents') ?? [];
   // console.log('scn got mode', app.getComponent('mode'), 'attached');
   const loadApp = (() => {
@@ -67,7 +68,10 @@ export default e => {
           if (components2.mode === undefined) {
             components2.mode = 'detached';
           }
-          
+          if (components2.paused === undefined) {
+            components2.paused = paused;
+          }
+
           const subApp = await createAppAsync({
             start_url: url,
             position,
@@ -79,6 +83,14 @@ export default e => {
           // app.add(subApp);
           // console.log('scn app add subapp', app, subApp, subApp.parent);
           // subApp.updateMatrixWorld();
+
+          app.addEventListener('componentsupdate', e => {
+            const {keys} = e;
+            if (keys.includes('paused')) {
+              const paused = app.getComponent('paused') ?? false;
+              subApp.setComponent('paused', paused);
+            }
+          });
         };
       }
       case 'attached': {
