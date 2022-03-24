cb2915026428ae57a9868fb7cc3740910a194b66
Avaer Kazmer <a@modules.io>
2022-03-24 02:42:36 -0400

    Make rendersettings app pause-aware

--- type_templates/rendersettings.js
+++ type_templates/rendersettings.js
@@ -10,49 +10,81 @@ export default e => {
   app.appType = 'rendersettings';
 
   const srcUrl = ${this.srcUrl};
-  const mode = app.getComponent('mode') ?? 'attached';
+  const paused = app.getComponent('paused') ?? false;
 
-  if (mode === 'attached') {
-    let live = true;
-    (async () => {
-      const res = await fetch(srcUrl);
-      if (!live) return;
-      const j = await res.json();
-      if (!live) return;
-      if (j) {
-        const {background} = j;
-        if (background) {
-          let {color} = background;
-          if (Array.isArray(color) && color.length === 3 && color.every(n => typeof n === 'number')) {
-            rootScene.background = new THREE.Color(color[0]/255, color[1]/255, color[2]/255);
-          }
+  let live = true;
+  let json = null;
+  (async () => {
+    const res = await fetch(srcUrl);
+    if (!live) return;
+    json = await res.json();
+    if (!live) return;
+
+    const paused = app.getComponent('paused') ?? false;
+    if (!paused) {
+      _bind();
+    }
+  })();
+
+  let bound = false;
+  const _bind = () => {
+    if (!bound) {
+      const {background} = json;
+      if (background) {
+        let {color} = background;
+        if (Array.isArray(color) && color.length === 3 && color.every(n => typeof n === 'number')) {
+          rootScene.background = new THREE.Color(color[0]/255, color[1]/255, color[2]/255);
         }
-        
-        const {fog} = j;
-        if (fog) {
-          if (fog.fogType === 'linear') {
-            const {args = []} = fog;
-            rootScene.fog = new THREE.Fog(new THREE.Color(args[0][0]/255, args[0][1]/255, args[0][2]/255).getHex(), args[1], args[2]);
-          } else if (fog.fogType === 'exp') {
-            const {args = []} = fog;
-            rootScene.fog = new THREE.FogExp2(new THREE.Color(args[0][0]/255, args[0][1]/255, args[0][2]/255).getHex(), args[1]);
-          } else {
-            console.warn('unknown rendersettings fog type:', fog.fogType);
-          }
-        }
-        
-        postProcessing.setPasses(j);
       }
-    })();
+      
+      const {fog} = json;
+      if (fog) {
+        if (fog.fogType === 'linear') {
+          const {args = []} = fog;
+          rootScene.fog = new THREE.Fog(new THREE.Color(args[0][0]/255, args[0][1]/255, args[0][2]/255).getHex(), args[1], args[2]);
+        } else if (fog.fogType === 'exp') {
+          const {args = []} = fog;
+          rootScene.fog = new THREE.FogExp2(new THREE.Color(args[0][0]/255, args[0][1]/255, args[0][2]/255).getHex(), args[1]);
+        } else {
+          console.warn('unknown rendersettings fog type:', fog.fogType);
+        }
+      }
+      
+      postProcessing.setPasses(j);
     
-    useCleanup(() => {
-      live = false;
+      bound = true;
+    }
+  };
+  const _unbind = () => {
+    if (bound) {
       rootScene.fog = null;
       rootScene.background = null;
 
       postProcessing.setPasses(null);
-    });
-  }
+    
+      bound = false;
+    }
+  };
+  
+  useCleanup(() => {
+    live = false;
+    _unbind();
+  });
+
+  /* if (!paused) {
+    _bind();
+  } */
+  app.addEventListener('componentsupdate', e => {
+    const {keys} = e;
+    if (keys.includes('paused')) {
+      const paused = app.getComponent('paused') ?? false;
+      if (paused) {
+        _unbind();
+      } else {
+        _bind();
+      }
+    }
+  });
 
   return app;
 };
