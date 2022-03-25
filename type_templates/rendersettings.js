ecf65543bf8874c2511a1536abbd1448902ed3ff
Avaer Kazmer <a@modules.io>
2022-03-25 01:38:19 -0400

    Rendersettings dead code cleanup

--- type_templates/rendersettings.js
+++ type_templates/rendersettings.js
@@ -5,18 +5,10 @@ const {useApp, useInternals, useRenderSettings, usePostProcessing, useCleanup} =
 export default e => {
   const app = useApp();
   const renderSettings = useRenderSettings();
-  // const postProcessing = usePostProcessing();
-  // const {rootScene} = useInternals();
 
   app.appType = 'rendersettings';
 
   const srcUrl = ${this.srcUrl};
-
-  /* const _isRenderable = () => {
-    const paused = app.getComponent('paused') ?? false;
-    const rendering = app.getComponent('rendering') ?? false;
-    return !paused || rendering;
-  }; */
 
   let live = true;
   let json = null;
@@ -27,23 +19,7 @@ export default e => {
     json = await res.json();
     if (!live) return;
     localRenderSettings = renderSettings.makeRenderSettings(json);
-
-    /* if (_isRenderable()) {
-      _bind();
-    } */
   })();
-
-  /* let bound = false;
-  const _bind = () => {
-    if (!bound) {
-      bound = true;
-    }
-  };
-  const _unbind = () => {
-    if (bound) {
-      bound = false;
-    }
-  }; */
   
   useCleanup(() => {
     live = false;
@@ -51,18 +27,6 @@ export default e => {
   });
 
   app.getRenderSettings = () => localRenderSettings;
-
-  /* app.addEventListener('componentsupdate', e => {
-    const {keys} = e;
-    if (keys.includes('paused') || keys.includes('rendering')) {
-      const renderable = _isRenderable();
-      if (renderable) {
-        _bind();
-      } else {
-        _unbind();
-      }
-    }
-  }); */
 
   return app;
 };
