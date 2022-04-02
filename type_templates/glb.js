c764dfae07236dda0ffa5f3d53e8de7d1485f87d
Avaer Kazmer <a@modules.io>
2022-04-02 06:08:40 -0400

    Remove pets functionality from GLB type template

--- type_templates/glb.js
+++ type_templates/glb.js
@@ -6,12 +6,12 @@ const {useApp, useFrame, useCleanup, useLocalPlayer, usePhysics, useLoaders, use
 // const wearableScale = 1;
 
 const localVector = new THREE.Vector3();
-const localVector2 = new THREE.Vector3();
+/* const localVector2 = new THREE.Vector3();
 const localQuaternion = new THREE.Quaternion();
 const localQuaternion2 = new THREE.Quaternion();
 const localQuaternion3 = new THREE.Quaternion();
 const localEuler = new THREE.Euler();
-const localMatrix = new THREE.Matrix4();
+const localMatrix = new THREE.Matrix4(); */
 
 // const z180Quaternion = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI);
 
@@ -36,14 +36,6 @@ export default e => {
   
   // glb state
   let animations;
-
-  // pet state
-  let petSpec = null;
-  let petMixer = null;
-  let idleAction = null;
-  let walkAction = null;
-  let runAction = null;
-  let rootBone = null;
   
   // sit state
   let sitSpec = null;
@@ -272,18 +264,11 @@ export default e => {
           o.castShadow = true;
           o.receiveShadow = true;
         }
-      });      
-      
-      if (petComponent) {
-        const m = _makePetMixer();
-        petMixer = m.petMixer;
-        idleAction = m.idleAction;
-      }
+      });
       
       activateCb = () => {
         if (
           app.getComponent('wear') ||
-          app.getComponent('pet') ||
           app.getComponent('sit')
         ) {
           app.wear();
@@ -293,17 +278,6 @@ export default e => {
   })());
   
   const _unwear = () => {
-    if (petSpec) {
-      petSpec = null;
-      petMixer.stopAllAction();
-      walkAction = null;
-      runAction = null;
-      rootBone = null;
-      
-      const m = _makePetMixer();
-      petMixer = m.petMixer;
-      idleAction = m.idleAction;
-    }
     if (sitSpec) {
       const sitAction = localPlayer.getAction('sit');
       if (sitAction) {
@@ -314,21 +288,7 @@ export default e => {
   app.addEventListener('wearupdate', e => {
     if (e.wear) {
       if (app.glb) {
-        const {animations} = app.glb;
-        
-        petSpec = app.getComponent('pet');
-        if (petSpec) {
-          const walkAnimation = (petSpec.walkAnimation && petSpec.walkAnimation !== petSpec.idleAnimation) ? animations.find(a => a.name === petSpec.walkAnimation) : null;
-          if (walkAnimation) {
-            walkAction = petMixer.clipAction(walkAnimation);
-            walkAction.play();
-          }
-          const runAnimation = (petSpec.runAnimation && petSpec.runAnimation !== petSpec.idleAnimation) ? animations.find(a => a.name === petSpec.runAnimation) : null;
-          if (runAnimation) {
-            runAction = petMixer.clipAction(runAnimation);
-            runAction.play();
-          }
-        }
+        // const {animations} = app.glb;
 
         sitSpec = app.getComponent('sit');
         if (sitSpec) {
@@ -358,8 +318,6 @@ export default e => {
     }
   });
   
-  const smoothVelocity = new THREE.Vector3();
-  const lastLookQuaternion = new THREE.Quaternion();
   const _getAppDistance = () => {
     const localPlayer = useLocalPlayer();
     const position = localVector.copy(localPlayer.position);
@@ -367,137 +325,7 @@ export default e => {
     const distance = app.position.distanceTo(position);
     return distance;
   };
-  const minDistance = 1;
-  const _isFar = distance => (distance - minDistance) > 0.01;
   useFrame(({timestamp, timeDiff}) => {
-    // components
-    const _updateAnimation = () => {
-      const petComponent = app.getComponent('pet');
-      if (petComponent) {
-        if (rootBone) {
-          rootBone.quaternion.copy(rootBone.originalQuaternion);
-          rootBone.updateMatrixWorld();
-        }
-        if (petMixer) { // animated pet
-          if (petSpec) { // activated pet
-            const speed = 0.0014;
-
-            const distance = _getAppDistance();
-            const moveDelta = localVector;
-            moveDelta.setScalar(0);
-            if (_isFar(distance)) { // handle rounding errors
-              const localPlayer = useLocalPlayer();
-              const position = localPlayer.position.clone();
-              position.y = 0;
-              const direction = position.clone()
-                .sub(app.position)
-                .normalize();
-              const maxMoveDistance = distance - minDistance;
-              const moveDistance = Math.min(speed * timeDiff, maxMoveDistance);
-              moveDelta.copy(direction)
-                .multiplyScalar(moveDistance);
-              app.position.add(moveDelta);
-              app.quaternion.slerp(localQuaternion.setFromUnitVectors(localVector2.set(0, 0, 1), direction), 0.1);
-              app.updateMatrixWorld();
-            } /* else {
-              // console.log('check', head === drop, component.attractedTo === 'fruit', typeof component.eatSpeed === 'number');
-              if (head === drop && component.attractedTo === 'fruit' && typeof component.eatSpeed === 'number') {
-                drop.scale.subScalar(1/component.eatSpeed*timeDiff);
-                // console.log('new scale', drop.scale.toArray());
-                if (drop.scale.x <= 0 || drop.scale.y <= 0 || drop.scale.z <= 0) {
-                  dropManager.removeDrop(drop);
-                }
-              }
-            } */
-            smoothVelocity.lerp(moveDelta, 0.3);
-            
-            const walkSpeed = 0.01;
-            const runSpeed = 0.03;
-            const currentSpeed = smoothVelocity.length();
-            if (walkAction) {
-              walkAction.weight = Math.min(currentSpeed / walkSpeed, 1);
-            }
-            if (runAction) {
-              runAction.weight = Math.min(Math.max((currentSpeed - walkSpeed) / (runSpeed - walkSpeed), 0), 1);
-            }
-            if (idleAction) {
-              if (walkAction || runAction) {
-                idleAction.weight = 1 - Math.min(currentSpeed / walkSpeed, 1);
-              } else {
-                idleAction.weight = 1;
-              }
-            }
-          } else { // unactivated pet
-            if (idleAction) {
-              idleAction.weight = 1;
-            }
-          }
-          const deltaSeconds = timeDiff / 1000;
-          petMixer.update(deltaSeconds);
-          petMixer.getRoot().updateMatrixWorld();
-        }
-      } else {
-        const deltaSeconds = timeDiff / 1000;
-        for (const mixer of hubsAnimationMixers) {
-          mixer.update(deltaSeconds);
-          app.updateMatrixWorld();
-        }
-      }
-    };
-    _updateAnimation();
-    
-    const _updateLook = () => {
-      const lookComponent = app.getComponent('look');
-      if (lookComponent && app.glb) {
-        let skinnedMesh = null;
-        app.glb.scene.traverse(o => {
-          if (skinnedMesh === null && o.isSkinnedMesh) {
-            skinnedMesh = o;
-          }
-        });
-        if (skinnedMesh) {
-          const bone = skinnedMesh.skeleton.bones.find(bone => bone.name === lookComponent.rootBone);
-          if (bone) {
-            rootBone = bone;
-            if (!bone.originalQuaternion) {
-              bone.originalQuaternion = bone.quaternion.clone();
-            }
-            if (!bone.originalWorldScale) {
-              bone.originalWorldScale = bone.getWorldScale(new THREE.Vector3());
-            }
-            
-            if (!bone.quaternion.equals(lastLookQuaternion)) {
-              const localPlayer = useLocalPlayer();
-              const {position, quaternion} = localPlayer;
-              localQuaternion2.setFromRotationMatrix(
-                localMatrix.lookAt(
-                  position,
-                  bone.getWorldPosition(localVector),
-                  localVector2.set(0, 1, 0)
-                )
-              ).premultiply(localQuaternion.copy(app.quaternion).invert());
-              localEuler.setFromQuaternion(localQuaternion2, 'YXZ');
-              localEuler.y = Math.min(Math.max(localEuler.y, -Math.PI*0.5), Math.PI*0.5);
-              localQuaternion2.setFromEuler(localEuler)
-                .premultiply(app.quaternion);
-              
-              bone.matrixWorld.decompose(localVector, localQuaternion, localVector2);
-              localQuaternion.copy(localQuaternion2)
-                .multiply(localQuaternion3.copy(bone.originalQuaternion).invert())
-                .normalize();
-              bone.matrixWorld.compose(localVector, localQuaternion, bone.originalWorldScale);
-              bone.matrix.copy(bone.matrixWorld)
-                .premultiply(localMatrix.copy(bone.parent.matrixWorld).invert())
-                .decompose(bone.position, bone.quaternion, bone.scale);
-              bone.updateMatrixWorld();
-              lastLookQuaternion.copy(bone.quaternion);
-            }
-          }
-        }
-      }
-    };
-    _updateLook();
-    
     // standards
     const _updateUvScroll = () => {
       for (const uvScroll of uvScrolls) {
