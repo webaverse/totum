52bcd4d9a3b021486581df81ad0019594c4cfd03
Avaer Kazmer <a@modules.io>
2022-05-11 03:33:30 -0400

    Major react type template work

--- type_templates/react.js
+++ type_templates/react.js
@@ -1,206 +1,91 @@
 import * as THREE from 'three';
 import metaversefile from 'metaversefile';
-const {useApp, useFrame, useLocalPlayer, useCleanup, /*usePhysics, */ useWorld} = metaversefile;
+const {useApp, useFrame, useDomRenderer, useInternals, useCleanup} = metaversefile;
+// import renderModule from ${this.srcUrl};
 
-const localVector = new THREE.Vector3();
-const localVector2 = new THREE.Vector3();
+let baseUrl = import.meta.url.replace(/(\\/)[^\\/\\\\]*$/, '$1');
+const bu = new URL(baseUrl);
+const proxyPrefix ='/@proxy/';
+if (bu.pathname.startsWith(proxyPrefix)) {
+  baseUrl = bu.pathname.slice(proxyPrefix.length) + bu.search + bu.hash;
+  console.log('replace base urll', {bu, baseUrl});
+} else {
+  console.log('does not start with', {baseUrl});
+}
+
+// const localVector = new THREE.Vector3();
+// const localVector2 = new THREE.Vector3();
+
+console.log('react app 0');
+
+window.fetch2 = fetch;
+window.import2 = s => import(s);
+
+/* // concatenates a new URL() object into a string, including everything like the protocol, search and hash parts
+const stringifyUrl = url => {
+  let s = '';
+  s += url.protocol + '//';
+  s += url.host + url.path;
+  s += url.search;
+  s += url.hash;
+  return s;
+}; */
 
 export default e => {
-  const app = useApp();
-
-  const srcUrl = ${this.srcUrl};
+  console.log('react app 1', e);
   
-  const worldLights = app;
-  app.light = null;
+  const app = useApp();
+  const {sceneLowerPriority} = useInternals();
+  const domRenderEngine = useDomRenderer();
 
-  const addShadows = (light, params) => {
-    light.castShadow = true; 
-    if (typeof params[1] === 'number') {
-      light.shadow.mapSize.width = params[1]; 
-      light.shadow.mapSize.height = params[1]; 
-    }
-    if (typeof params[2] === 'number') {
-      light.shadow.camera.near = params[2];
-    }
-    if (typeof params[3] === 'number') {
-      light.shadow.camera.far = params[3];
-    }
-    if (typeof params[0] === 'number') {
-      light.shadow.camera.left = params[0];
-      light.shadow.camera.right = -params[0];
-      light.shadow.camera.top = params[0];
-      light.shadow.camera.bottom = -params[0];
-    }
-    if (typeof params[4] === 'number') {
-      light.shadow.bias = params[4];
-    }
-    if (typeof params[5] === 'number') {
-      light.shadow.normalBias = params[5];
-    }
-    
-    light.shadow.camera.initialLeft = light.shadow.camera.left;
-    light.shadow.camera.initialRight = light.shadow.camera.right;
-    light.shadow.camera.initialTop = light.shadow.camera.top;
-    light.shadow.camera.initialBottom = light.shadow.camera.bottom;
-    
-    // light.params = params;
-    // console.log("Added shadows for:", light, "with params:", params);
-  };
+  let srcUrl = ${this.srcUrl};
+  // srcUrl = new URL(srcUrl, window.location.href).href + '?noimport=1';
 
-  let json = null;
+  console.log('react app 2', domRenderEngine, srcUrl);
+
+  let dom = null;
   e.waitUntil((async () => {
     const res = await fetch(srcUrl);
-    json = await res.json();
+    const json = await res.json();
+    let {jsxUrl} = json;
+    console.log('react app 3.1', {jsxUrl, json, baseUrl});
+    jsxUrl = new URL(jsxUrl, baseUrl).href;
+    console.log('react app 3.2', {jsxUrl, json, baseUrl});
+    // jsxUrl = './chest/' + jsxUrl.replace(/^\\.\\//g, '');
+    // console.log('react app 3.3', {jsxUrl, json, baseUrl});
+    const m = await import(jsxUrl);
+    console.log('react app 3.3', jsxUrl, m);
+  
+    dom = domRenderEngine.addDom({
+      render: () => m.default(),
+    });
+    console.log('react app 5', jsxUrl, m, app, dom);
 
-    _render();
+    sceneLowerPriority.add(dom);
+    dom.updateMatrixWorld();
+
+    console.log('react app 5', jsxUrl, m, app, dom, sceneLowerPriority);
   })());
 
-  const lightTrackers = [];
-  const lightTargets = [];
-  const _render = () => {
-    if (json !== null) {
-      let {lightType, args, position, shadow} = json;
-      const light = (() => {
-        switch (lightType) {
-          case 'ambient': {
-            return new THREE.AmbientLight(
-              new THREE.Color().fromArray(args[0]).multiplyScalar(1/255).getHex(),
-              args[1]
-            );
-          }
-          case 'directional': {
-            return new THREE.DirectionalLight(
-              new THREE.Color().fromArray(args[0]).multiplyScalar(1/255).getHex(),
-              args[1]
-            );
-          }
-          case 'point': {
-            return new THREE.PointLight(
-              new THREE.Color().fromArray(args[0]).multiplyScalar(1/255).getHex(),
-              args[1],
-              args[2],
-              args[3]
-            );
-          }
-          case 'spot': {
-            return new THREE.SpotLight(
-              new THREE.Color().fromArray(args[0]).multiplyScalar(1/255).getHex(),
-              args[1],
-              args[2],
-              args[3],
-              args[4],
-              args[5]
-            );
-          }
-          case 'rectArea': {
-            return new THREE.RectAreaLight(
-              new THREE.Color().fromArray(args[0]).multiplyScalar(1/255).getHex(),
-              args[1],
-              args[2],
-              args[3]
-            );
-          }
-          case 'hemisphere': {
-            return new THREE.HemisphereLight(
-              new THREE.Color().fromArray(args[0]).multiplyScalar(1/255).getHex(),
-              new THREE.Color().fromArray(args[1]).multiplyScalar(1/255).getHex(),
-              args[2]
-            );
-          }
-          default: {
-            return null;
-          }
-        }
-      })();
-      if (light) {
-        light.lastAppMatrixWorld = new THREE.Matrix4();
-        light.plane = new THREE.Plane().setFromNormalAndCoplanarPoint(new THREE.Vector3(0, -1, 0), app.position);
-
-        if (lightType === 'directional' || lightType === 'point' || lightType === 'spot') {
-          if (Array.isArray(shadow)) {
-            addShadows(light, shadow);
-          }
-        }
-        
-        const lightTracker = new THREE.Object3D();
-        lightTracker.name = 'LightTracker';
-        if (Array.isArray(position)) {
-          lightTracker.position.fromArray(position);
-        } else {
-          lightTracker.position.set(0, 0, 0);
-        }
-        light.position.set(0, 0, 0);
-        lightTracker.add(light);
-        lightTracker.light = light;
-        
-        worldLights.add(lightTracker);
-        lightTrackers.push(lightTracker)
-        if (light.target) {
-          worldLights.add(light.target);
-          lightTargets.push(light.target);
-        }
-        lightTracker.updateMatrixWorld(true);
-        
-        app.light = lightTracker;
-      } else {
-        console.warn('invalid light spec:', j);
-      }
-    }
-  };
-
   useFrame(() => {
-    if (lightTrackers.length > 0) {
-      for (const lightTracker of lightTrackers) {
-        const {light} = lightTracker;
-        if (!light.lastAppMatrixWorld.equals(app.matrixWorld)) {
-          light.position.copy(app.position);
-          // light.quaternion.copy(app.quaternion);
-          if (light.target) {
-            light.quaternion.setFromRotationMatrix(
-              new THREE.Matrix4().lookAt(
-                light.position,
-                light.target.position,
-                localVector.set(0, 1, 0),
-              )
-            );
-          }
-          light.scale.copy(app.scale);
-          light.matrix.copy(app.matrix);
-          light.matrixWorld.copy(app.matrixWorld);
-          light.lastAppMatrixWorld.copy(app.matrixWorld);
-          light.updateMatrixWorld();
-        }
-      }
-
-      const localPlayer = useLocalPlayer();
-      for (const lightTracker of lightTrackers) {
-        const {light} = lightTracker;
-        if (light.isDirectionalLight) {
-          light.plane.setFromNormalAndCoplanarPoint(localVector.set(0, 0, -1).applyQuaternion(light.shadow.camera.quaternion), light.shadow.camera.position);
-          const planeTarget = light.plane.projectPoint(localPlayer.position, localVector);
-          // light.updateMatrixWorld();
-          const planeCenter = light.shadow.camera.position.clone();
-          
-          const x = planeTarget.clone().sub(planeCenter)
-            .dot(localVector2.set(1, 0, 0).applyQuaternion(light.shadow.camera.quaternion));
-          const y = planeTarget.clone().sub(planeCenter)
-            .dot(localVector2.set(0, 1, 0).applyQuaternion(light.shadow.camera.quaternion));
-          
-          light.shadow.camera.left = x + light.shadow.camera.initialLeft;
-          light.shadow.camera.right = x + light.shadow.camera.initialRight;
-          light.shadow.camera.top = y + light.shadow.camera.initialTop;
-          light.shadow.camera.bottom = y + light.shadow.camera.initialBottom;
-          light.shadow.camera.updateProjectionMatrix();
-          light.updateMatrixWorld();
-        }
-      }
+    if (dom) {
+      app.matrixWorld.decompose(dom.position, dom.quaternion, dom.scale);
+      // console.log('update dom', dom.position.toArray().join(','), dom.quaternion.toArray().join(','), dom.scale.toArray().join(','));
+      // dom.position.copy(app.position);
+      // dom.quaternion.copy(app.quaternion);
+      // dom.scale.copy(app.scale);
+      // dom.matrix.copy(app.matrix);
+      // dom.matrixWorld.copy(app.matrixWorld);
+      dom.updateMatrixWorld();
     }
   });
+
+  console.log('react app 4');
 
   return app;
 };
 export const contentId = ${this.contentId};
 export const name = ${this.name};
 export const description = ${this.description};
-export const type = 'light';
+export const type = 'react';
 export const components = ${this.components};
