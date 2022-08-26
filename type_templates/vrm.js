c6bdbce0b0c27c4f3fd36a5ee239841dda3af053
Jack Z <jDevlin685@outlook.com>
2022-08-26 21:54:02 +0800

    Revert vrm avatar quality setting

--- type_templates/vrm.js
+++ type_templates/vrm.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import metaversefile from 'metaversefile';
-const {useApp, usePhysics, useAvatarRenderer, useCamera, useCleanup, useActivate, useLocalPlayer, useCharacterQuality} = metaversefile;
+const {useApp, usePhysics, useAvatarRenderer, useCamera, useCleanup, useActivate, useLocalPlayer} = metaversefile;
 
 const localVector = new THREE.Vector3();
 const localVector2 = new THREE.Vector3();
@@ -35,7 +35,6 @@ export default e => {
     avatarRenderer = new AvatarRenderer({
       arrayBuffer,
       srcUrl,
-      quality: useCharacterQuality(),
       camera,
       isVrm: true,
     });
