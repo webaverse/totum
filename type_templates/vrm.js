5b440344f1e6e2ca35514338300452ca7918e8ec
Jack Z <jDevlin685@outlook.com>
2022-09-07 02:33:25 -0700

    fix vrm object rendering

--- type_templates/vrm.js
+++ type_templates/vrm.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import metaversefile from 'metaversefile';
-const {useApp, usePhysics, useAvatarRenderer, useCamera, useCleanup, useActivate, useLocalPlayer} = metaversefile;
+const {useApp, usePhysics, useAvatarRenderer, useCamera, useCleanup, useFrame, useActivate, useLocalPlayer} = metaversefile;
 
 const localVector = new THREE.Vector3();
 const localVector2 = new THREE.Vector3();
@@ -28,6 +28,7 @@ export default e => {
   let avatarRenderer = null;
   let physicsIds = [];
   let activateCb = null;
+  let frameCb = null;
   e.waitUntil((async () => {
     const arrayBuffer = await _fetchArrayBuffer(srcUrl);
     
@@ -90,10 +91,21 @@ export default e => {
       const localPlayer = useLocalPlayer();
       localPlayer.setAvatarApp(app);
     };
+
+    const {height} = avatarRenderer.getAvatarSize();
+    frameCb = ({timestamp, timeDiff}) => {
+      if (!avatarRenderer.isControlled) {
+        avatarRenderer.updateObject(timestamp, timeDiff, height);
+      }
+    };
   })());
 
   useActivate(() => {
     activateCb && activateCb();
+  });
+
+  useFrame((e) => {
+    frameCb && frameCb(e);
   });
 
   // controlled tracking
