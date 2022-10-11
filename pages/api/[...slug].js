e72858b81c07db1677b9807c255170dd070887e1
Avaer Kazmer <a@modules.io>
2022-10-10 21:32:45 -0400

    Add content-type

--- pages/api/[...slug].js
+++ pages/api/[...slug].js
@@ -11,6 +11,7 @@ export default async function handler(req, res) {
       u = u.replace(/^([a-zA-Z0-9]+:\/(?!\/))/, '$1/');
       const resultUint8Array = await compile(u);
       const resultBuffer = Buffer.from(resultUint8Array);
+      res.setHeader('Content-Type', 'application/javascript');
       res.end(resultBuffer);
     } else {
       res.status(404).send('not found');
