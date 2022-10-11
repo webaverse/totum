ac583324518d7fad693fb0d634289cb9ca9a449b
Avaer Kazmer <a@modules.io>
2022-10-10 21:32:39 -0400

    Add CORS headers

--- next.config.js
+++ next.config.js
@@ -1,8 +1,37 @@
 /** @type {import('next').NextConfig} */
+
+const securityHeaders = [
+  {
+    key: 'Access-Control-Allow-Origin',
+    value: '*',
+  },
+  {
+    key: 'Cross-Origin-Opener-Policy',
+    value: 'same-origin',
+  },
+  {
+    key: 'Cross-Origin-Embedder-Policy',
+    value: 'require-corp',
+  },
+  {
+    key: 'Cross-Origin-Resource-Policy',
+    value: 'cross-origin',
+  },
+]
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   trailingSlash: true,
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
+  },
   async rewrites() {
     return [
       {
